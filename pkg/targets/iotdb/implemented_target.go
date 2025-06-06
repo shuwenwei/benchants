c96d54f1b19d35e6f5465e00acd375278589487d
shuwenwei <s13979062948@gmail.com>
2025-06-06 15:19:48 +0800

    fix

--- pkg/targets/iotdb/implemented_target.go
+++ pkg/targets/iotdb/implemented_target.go
@@ -35,6 +35,7 @@ func (t *iotdbTarget) TargetSpecificFlags(flagPrefix string, flagSet *pflag.Flag
 	flagSet.Int(flagPrefix+"channel-capacity", 0, "Capacity of channel")
 	flagSet.Bool(flagPrefix+"flow-control", true, "Flow Control")
 	flagSet.Int(flagPrefix+"tablet-size", 20, "Tablet Size")
+	flagSet.Bool(flagPrefix+"init", true, "auto create timeseries")
 }
 
 func (t *iotdbTarget) TargetName() string {
